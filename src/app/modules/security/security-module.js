d39b9ee44be1e8cf561e9f33e44877a8631f9d72
malsjalbl <mike.langley@acuteit.co.uk>
2016-02-10 18:50:54 +0000

    None

--- src/app/modules/security/security-module.js
+++ src/app/modules/security/security-module.js
@@ -2,9 +2,9 @@
  * Created by mike on 05/11/2015.
  */
 //import {SecurityController} from './security-controller';
-import {SecurityService}        from './security-service';
+import {SecurityService} from './security-service';
 //import {securityConfig}         from "./security-manConfig.js";
-import {securityRouteConfig }   from "./security-route.js";
+import {securityRouteConfig } from "./security-route.js";
 //import {securityRunBlock}       from "./security-man-run.js";
 
 export var appSecurityModule = angular.module('appSecurityModule', ['ui.router'])
